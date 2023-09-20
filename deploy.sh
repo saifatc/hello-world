366e8dbd1afd4743a411f9ef3aff2f3f74255496
Saif Rahman <mrahman@australianturfclub.com.au>
2023-09-21 01:37:00 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-sudo apt-get update
+sudo rm -rf /var/www/html
+sudo mkdir /var/www/html
+cd /var/www/html
+sudo cp -R $(System.DefaultWorkingDirectory)/$(Release.PrimaryArtifactSourceAlias)/drop/. .
